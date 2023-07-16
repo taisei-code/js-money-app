ce02d8ce098a3bf498b528a64f2a24a279f0f8bd
Taisei <taisei96.web@gmail.com>
2023-07-16 13:44:54 +0900

    データベースの巣性

--- indexedDB.js
+++ indexedDB.js
@@ -1,1 +1,28 @@
 //第4章：収支のデータをデータベースに記録
+
+//indexedDBの名前などの設定
+const dbName = "kakeiboDB";
+const storeName = "kakeiboStore";
+const dbVersion = 1;
+
+//データベース接続する。データベースが未作成なら新規作成する。
+let database = indexedDB.open(dbName, dbVersion);
+
+ //データベースとオブジェクトストアの作成
+ database.onupgradeneeded = function (event) {
+     let db = event.target.result;
+     db.createObjectStore(storeName, { keyPath: "id" });
+     console.log("データベースを新規作成しました");
+ }
+
+ // データベースの接続に成功したときに発生するイベント
+database.onsuccess = function (event) {
+  let db = event.target.result;
+  // 接続を解除する
+  db.close();
+  console.log("データベースに接続できました");
+}
+ 
+database.onerror = function (event) {
+  console.log("データベースに接続できませんでした");
+}
